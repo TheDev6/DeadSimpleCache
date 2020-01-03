88fca5af633c283e4e9a4c6690def85a6fd9087b
Allan Chadwick <4chadwick@gmail.com>
2020-01-03 12:11:58 -0700

    Fix Comment.

--- src/DeadSimpleCache/SimpleCacheResult.cs
+++ src/DeadSimpleCache/SimpleCacheResult.cs
@@ -3,7 +3,7 @@
     public class SimpleCacheResult<T>
     {
         /// <summary>
-        /// Always check for cache null, especially for nullable value types like false(not nullable) bool where the default would give the incorrect answer of false, when the cache was actually empty.
+        /// Always check for cache null, especially for value types like false(not nullable) bool where the default would give the incorrect answer of false, when the cache was actually empty.
         /// </summary>
         public bool IsCacheNull { get; set; }
         public T ValueOrDefault { get; set; }
